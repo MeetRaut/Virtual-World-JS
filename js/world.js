a4c447bbae4dda9a75c8075819b5d9801cbc9a09
Meet Raut <meetraut3004@gmail.com>
2024-06-23 19:43:12 +0530

    add 3d trees

--- js/world.js
+++ js/world.js
@@ -73,7 +73,7 @@ class World {
       // check if tree too close to other trees
       if(keep){
         for(const tree of trees){
-          if(distance(tree, p) < this.treeSize){
+          if(distance(tree.center, p) < this.treeSize){
             keep = false;
             break;
           }
@@ -93,7 +93,7 @@ class World {
       }
 
       if(keep){
-        trees.push(p);
+        trees.push(new Tree(p, this.treeSize));
         tryCount = 0;
       }
       tryCount++;
@@ -164,7 +164,7 @@ class World {
     return bases;
   }
 
-  draw(ctx) {
+  draw(ctx, viewPoint) {
     for (const env of this.envelopes) {
       env.draw(ctx, { fill: "#BBB", stroke: "#BBB", lineWidth: 15 });
     }
@@ -176,7 +176,7 @@ class World {
     }
 
     for(const tree of this.trees){
-      tree.draw(ctx, { size: this.treeSize, color: "rgba(0,0,0,0.5"});
+      tree.draw(ctx, viewPoint);
     }
     for(const bld of this.buildings){
       bld.draw(ctx);
